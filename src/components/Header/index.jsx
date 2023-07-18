d80cf147e6cd57291595ab10303acbe7011a748f
NikolaJelavic <jelavicnikola@gmail.com>
2023-07-18 19:30:53 +0200

    favicon, header and navbar

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+
+export default function index() {
+    
+
+    return(
+        <div className="flex">
+            <img src={"../../../img/monuments/Podgaric/podgaric.jpg"} alt="podgaric" className="w-1/12 h-20" />
+            <img src={"../../../img/monuments/Kosmaj/kosmaj.png"} alt="kosmaj" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Tjentiste/Tjentiste.png"} alt="tjentiste" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Kozara/kozara.png"} alt="kozara" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Ilirska Bistrica/ilirskaBistrica.png"} alt="ilirskaBistrica" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Sisak/sisak.png"} alt="sisak" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Lepoglava/lepoglava.jpg"} alt="lepoglava" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Kragujevac/sumarice.jpg"} alt="sumarice" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Botun/botun.jpg"} alt="botun" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Belcista/belcista.jpg"} alt="belcista" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Lukovdol/lukovDol.jpg"} alt="lukovdol" className="w-1/12 h-20"/>
+            <img src={"../../../img/monuments/Obadov Brijeg/obadovBrijeg1.jpg"} alt="obadov brijeg" className="w-1/12 h-20"/>
+        </div>
+    )
+};
