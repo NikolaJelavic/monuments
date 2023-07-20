ff65def84460942f0a26e196b79d8ae8a3d1ee1b
NikolaJelavic <jelavicnikola@gmail.com>
2023-07-20 10:35:53 +0200

    fixed vercel

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -37,18 +37,18 @@ export default function index() {
 
     return(
         <div className="flex">
-             <Image src={"/img/monuments/Podgaric/podgaric.jpg"} alt="podgaric"  width={500} height={500}        className="       sm:w-1/4 md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Kosmaj/kosmaj.png"} alt="kosmaj"  width={500}  height={500}             className="       sm:w-1/4 md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Tjentiste/Tjentiste.png"} alt="tjentiste" width={500} height={500}      className="       sm:w-1/4 md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Ilirska Bistrica/ilirskaBistrica.png"}  width={500} height={500}        className="       sm:w-1/4 md:w-1/12 lg:w-1/12  h-20 "      alt="ilirskaBistrica"/>
-             <Image src={"/img/monuments/Sisak/sisak.png"} alt="sisak" width={500}   height={500}                className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Lepoglava/lepoglava.jpg"} alt="lepoglava" width={500}   height={500}    className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Kozara/kozara.png"} alt="kozara" width={500}  height={500}              className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Kragujevac/sumarice.jpg"} alt="sumarice" width={500}  height={500}      className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Botun/botun.jpg"} alt="botun" width={500}   height={500}                className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Belcista/belcista.jpg"} alt="belcista" width={500}   height={500}       className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Lukovdol/lukovDol.jpg"} alt="lukovdol" width={500}   height={500}       className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 "/>
-             <Image src={"/img/monuments/Obadov Brijeg/obadovBrijeg1.jpg"}  width={500}  height={500}            className="hidden sm:block md:w-1/12 lg:w-1/12  h-20 " alt="obadov brijeg"/>
+<Image src={"/img/monuments/Podgaric/podgaric.jpg"} alt="podgaric" width={500} height={500} className="sm:w-1/4 md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Kosmaj/kosmaj.png"} alt="kosmaj" width={500} height={500} className="sm:w-1/4 md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Tjentiste/Tjentiste.png"} alt="tjentiste" width={500} height={500} className="sm:w-1/4 md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Ilirska Bistrica/ilirskaBistrica.png"} width={500} height={500} className="sm:w-1/4 md:w-1/12 lg:w-1/12  h-20" alt="ilirskaBistrica"/>
+<Image src={"/img/monuments/Sisak/sisak.png"} alt="sisak" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Lepoglava/lepoglava.jpg"} alt="lepoglava" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Kozara/kozara.png"} alt="kozara" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Kragujevac/sumarice.jpg"} alt="sumarice" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Botun/botun.jpg"} alt="botun" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Belcista/belcista.jpg"} alt="belcista" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Lukovdol/lukovDol.jpg"} alt="lukovdol" width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20"/>
+<Image src={"/img/monuments/Obadov Brijeg/obadovBrijeg1.jpg"} width={500} height={500} className="hidden sm:block md:w-1/12 lg:w-1/12  h-20" alt="obadov brijeg"/>
         </div>
             )
         };
