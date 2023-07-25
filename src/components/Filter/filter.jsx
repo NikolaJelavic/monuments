1810082f8a08782715b70452ae22e3f0a72cc6e2
NikolaJelavic <jelavicnikola@gmail.com>
2023-07-25 03:32:16 +0200

    ffirst step, radio buttons

---
+++ src/components/Filter/filter.jsx
@@ -0,0 +1,45 @@
+export default function Filter() {
+    
+
+    return <>
+        <form action="#">
+  <p>
+    <input type="radio" id="yugoslavia" name="radio-group" checked/>
+    <label for="yugoslavia">Yugoslavia</label>
+  </p>
+  <p>
+    <input type="radio" id="Serbia" name="radio-group"/>
+    <label for="Serbia">Serbia</label>
+  </p>
+  <p>
+    <input type="radio" id="Croatia" name="radio-group"/>
+    <label for="Croatia">Croatia</label>
+  </p>
+  <p>
+    <input type="radio" id="Bosnia" name="radio-group"/>
+    <label for="Bosnia">Bosnia and Hercegovina</label>
+  </p>
+  <p>
+    <input type="radio" id="SLovenia" name="radio-group"/>
+    <label for="SLovenia">Slovenia</label>
+  </p>
+  <p>
+    <input type="radio" id="Macedonia" name="radio-group"/>
+    <label for="Macedonia">Macedonia</label>
+  </p>
+  <p>
+    <input type="radio" id="Montenegro" name="radio-group"/>
+    <label for="Montenegro">Montenegro</label>
+  </p>
+  <p>
+    <input type="radio" id="Vojvodina" name="radio-group"/>
+    <label for="Vojvodina">Vojvodina</label>
+  </p>
+  <p>
+    <input type="radio" id="Kosovo" name="radio-group"/>
+    <label for="Kosovo">Kosovo</label>
+  </p>
+
+</form>
+</>
+};
