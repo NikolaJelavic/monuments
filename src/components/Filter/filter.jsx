dbdbe2a1b08aa6c9c91e8ef5d4b3b9744ba93d1f
NikolaJelavic <jelavicnikola@gmail.com>
2023-07-26 21:30:29 +0200

    filtering

--- src/components/Filter/filter.jsx
+++ src/components/Filter/filter.jsx
@@ -1,45 +1,101 @@
-export default function Filter() {
-    
+import React, { useState } from 'react';
 
-    return <>
-        <form action="#">
-  <p>
-    <input type="radio" id="yugoslavia" name="radio-group" checked/>
-    <label for="yugoslavia">Yugoslavia</label>
-  </p>
-  <p>
-    <input type="radio" id="Serbia" name="radio-group"/>
-    <label for="Serbia">Serbia</label>
-  </p>
-  <p>
-    <input type="radio" id="Croatia" name="radio-group"/>
-    <label for="Croatia">Croatia</label>
-  </p>
-  <p>
-    <input type="radio" id="Bosnia" name="radio-group"/>
-    <label for="Bosnia">Bosnia and Hercegovina</label>
-  </p>
-  <p>
-    <input type="radio" id="SLovenia" name="radio-group"/>
-    <label for="SLovenia">Slovenia</label>
-  </p>
-  <p>
-    <input type="radio" id="Macedonia" name="radio-group"/>
-    <label for="Macedonia">Macedonia</label>
-  </p>
-  <p>
-    <input type="radio" id="Montenegro" name="radio-group"/>
-    <label for="Montenegro">Montenegro</label>
-  </p>
-  <p>
-    <input type="radio" id="Vojvodina" name="radio-group"/>
-    <label for="Vojvodina">Vojvodina</label>
-  </p>
-  <p>
-    <input type="radio" id="Kosovo" name="radio-group"/>
-    <label for="Kosovo">Kosovo</label>
-  </p>
+export default function Filter({ data, selectedState, setSelectedState }) {
+  const [selectedOption, setSelectedOption] = useState('yugoslavia');
 
-</form>
-</>
-};
+  const handleOptionClick = (option) => {
+    if (option === 'yugoslavia') {
+      setSelectedState(null); 
+    } else {
+      setSelectedState(option);
+    }
+    setSelectedOption(option);
+  };
+
+  const filterMonuments = (monuments) => {
+    console.log("Selected State:", selectedState);
+
+    if (!selectedState) return monuments;
+
+    return monuments.filter((monument) => {
+      // Use case-insensitive comparison by converting both values to lowercase
+      const monumentState = monument.state ? monument.state.toLowerCase() : '';
+      const selectedStateLower = selectedState.toLowerCase();
+
+      if (monumentState === selectedStateLower) return true;
+
+      // Check if the monument's province matches the selected state
+      const monumentProvince = monument.province ? monument.province.toLowerCase() : '';
+      if (monumentProvince === selectedStateLower) return true;
+
+      return false;
+    });
+  };
+
+  // Apply filtering logic to the monuments
+  const filteredMonuments = filterMonuments(data);
+
+  console.log("Filtered Monuments:", filteredMonuments);
+
+  return (
+    <>
+      <div className='bg-red-400 flex flex-col w-24 rounded'>
+        <p className='w-40 bg-gray-400 rounded p-2'>Filter monuments by state or province</p>
+        <button
+          className={selectedOption === 'yugoslavia' ? 'active' : ''}
+          onClick={() => handleOptionClick('yugoslavia')}
+        >
+          Yugoslavia
+        </button>
+        <button
+          className={selectedOption === 'Serbia' ? 'active' : ''}
+          onClick={() => handleOptionClick('Serbia')}
+        >
+          Serbia
+        </button>
+        <button
+          className={selectedOption === 'Croatia' ? 'active' : ''}
+          onClick={() => handleOptionClick('Croatia')}
+        >
+          Croatia
+        </button>
+        <button
+          className={selectedOption === 'Bosnia' ? 'active' : ''}
+          onClick={() => handleOptionClick('Bosnia')}
+        >
+          B&H
+        </button>
+        <button
+          className={selectedOption === 'Slovenia' ? 'active' : ''}
+          onClick={() => handleOptionClick('Slovenia')}
+        >
+          Slovenia
+        </button>
+        <button
+          className={selectedOption === 'Macedonia' ? 'active' : ''}
+          onClick={() => handleOptionClick('Macedonia')}
+        >
+          Macedonia
+        </button>
+        <button
+          className={selectedOption === 'Montenegro' ? 'active' : ''}
+          onClick={() => handleOptionClick('Montenegro')}
+        >
+          Montenegro
+        </button>
+        <button
+          className={selectedOption === 'vojvodina' ? 'active' : ''}
+          onClick={() => handleOptionClick('vojvodina')} // Updated to lowercase 'vojvodina'
+        >
+          Vojvodina
+        </button>
+        <button
+          className={selectedOption === 'kosovo' ? 'active' : ''}
+          onClick={() => handleOptionClick('kosovo')} // Updated to lowercase 'kosovo'
+        >
+          Kosovo
+        </button>
+      </div>
+    </>
+  );
+}
