2aac8a4d4bf220fc458e7468c002ce81134160d7
NikolaJelavic <jelavicnikola@gmail.com>
2023-07-29 15:27:20 +0200

    Contact working

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,62 +1,135 @@
 /* eslint-disable react/no-unescaped-entities */
-import Header from "../../components/Header"
-import Navbar from "../navbar/navbar"
-
-
+import Header from "../../components/Header";
+import Navbar from "../navbar/navbar";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Rest() {
-    
-    
-    return (
-      <>
-      <Header/>
-      <Navbar/>
-      
-     <div className="flex justify-center mt-10 mb-10">
-        <form className="w-full max-w-3xl ">
-        <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2" for="grid-first-name">
-                First Name
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Josip" autofocus />
-            {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2 " for="grid-last-name">
-                Last Name
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Broz" />
-            </div>
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_dqjbut5",
+        "template_87nnek8",
+        form.current,
+        "jD_xZvBkxFpJS5k70"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="text-2xl mt-8 font-bold w-1/2 text-center">
+          Feel free to get in contact via email "nob-spomenici@gmail.com" or the
+          form below.
+        </h1>
+        <h1 className="text-2xl m-1 font-bold w-1/2 text-center">
+          Also, If you know about some monument which is not on the map, feel
+          free to send name and coordinates (http://bboxfinder.com/).
+        </h1>
+      </div>
+
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="max-w-md mx-auto p-4 bg-white shadow-lg rounded-lg my-10"
+      >
+        <div className="mb-4">
+          <label className="block text-gray-600 font-semibold">Name</label>
+          <input
+            type="text"
+            name="user_name"
+            className="w-full px-4 py-2 mt-2 text-gray-800 bg-gray-100 border rounded focus:outline-none focus:border-blue-500"
+            placeholder="Josip"
+            autofocus
+          />
         </div>
-        <div clasNames="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full ">
-            <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2" for="grid-password">
-                E-mail
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email"  placeholder="josipbroz@nam.org"/>
-            <p class="text-gray-600 text-xs italic">We won't misuse your email, promise!</p>
-            </div>
+        <div className="mb-4">
+          <label className="block text-gray-600 font-semibold">Email</label>
+          <input
+            type="email"
+            name="user_email"
+            className="w-full px-4 py-2 mt-2 text-gray-800 bg-gray-100 border rounded focus:outline-none focus:border-blue-500"
+            placeholder="josipbroz@nam.org"
+          />
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-            <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2" for="grid-password">
-                Message
-            </label>
-            <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border  border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-            {/* <p className="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none / resize-y / resize-x / resize</p> */}
-            </div>
+        <div className="mb-4">
+          <label className="block text-gray-600 font-semibold">Message</label>
+          <textarea
+            name="message"
+            className="w-full px-4 py-2 mt-2 text-gray-800 bg-gray-100 border rounded focus:outline-none focus:border-blue-500"
+            placeholder="Smrt fasizmu sloboda narodu!"
+          />
         </div>
-        <div class="md:flex md:items-center">
-            <div className="md:w-1/3">
-            <button className="shadow bg-red-600 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                Send
-            </button>
-            </div>
-            {/* <div class="md:w-2/3">text</div> */}
+        <div className="text-center">
+          <input
+            type="submit"
+            value="Send"
+            className="w-full px-4 py-2 text-lg font-semibold text-white bg-red-500 rounded hover:bg-red-600 focus:outline-none focus:bg-blue-600"
+          />
         </div>
-        </form>
-        </div>
-      </>
-    )
-};
+      </form>
+    </>
+  );
+}
+
+//  <div className="flex justify-center mt-10 mb-10">
+//     <form className="w-full max-w-3xl ">
+//     <div className="flex flex-wrap -mx-3 mb-6">
+//         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+//         <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2" for="grid-first-name">
+//             First Name
+//         </label>
+//         <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Josip" autofocus />
+//         {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
+//         </div>
+//         <div className="w-full md:w-1/2 px-3">
+//         <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2 " for="grid-last-name">
+//             Last Name
+//         </label>
+//         <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Broz" />
+//         </div>
+//     </div>
+//     <div clasNames="flex flex-wrap -mx-3 mb-6">
+//         <div className="w-full ">
+//         <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2" for="grid-password">
+//             E-mail
+//         </label>
+//         <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email"  placeholder="josipbroz@nam.org"/>
+//         <p class="text-gray-600 text-xs italic">We won't misuse your email, promise!</p>
+//         </div>
+//     </div>
+//     <div class="flex flex-wrap -mx-3 mb-6">
+//         <div class="w-full px-3">
+//         <label className="block uppercase tracking-wide text-gray-700 text-md font-bold mb-2" for="grid-password">
+//             Message
+//         </label>
+//         <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border  border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
+//         {/* <p className="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none / resize-y / resize-x / resize</p> */}
+//         </div>
+//     </div>
+//     <div class="md:flex md:items-center">
+//         <div className="md:w-1/3">
+//         <button className="shadow bg-red-600 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+//             Send
+//         </button>
+//         </div>
+//         {/* <div class="md:w-2/3">text</div> */}
+//     </div>
+//     </form>
+//     </div>
--- src/pages/quotes/index.js
+++ src/pages/quotes/index.js
@@ -1,23 +1,82 @@
-import Header from "../../../src/components/Header"
-import Navbar from "../navbar/navbar"
+import Header from "../../../src/components/Header";
+import Navbar from "../navbar/navbar";
 
+import React, { useState } from "react";
 
+// import { FaQuoteLeft } from "react-icons/fa";
+// &quot;
+const quotes = [
+  {
+    quote: "''We are brothers in blood and battle.''",
+    author: "Josip Broz Tito,",
+  },
+  {
+    quote: "''In war, there are no unwounded soldiers.''",
+    author: "Josip Broz Tito",
+  },
+  {
+    quote:
+      "''Unity and brotherhood of our nations and nationalities is the rock upon which we have built our state.''",
+    author: "Josip Broz Tito",
+  },
+  {
+    quote: "''We have shown that we are able to fight against the oppressors in war and win, and that we know how to build a new, just, and free world in peace.''",
+    author: "Edvard Kardelj",
+  },
+  {
+    quote: "''We fight in the name of human dignity, which knows no compromise or retreat.''",
+    author: "Josip Broz Tito",
+  },
+  {
+    quote: "''Everyone who can think and see beyond their own nose and doorstep must be clear that without their servants - Chetniks and Ustashe - the position of the occupiers would already be unsustainable and unbearable.''",
+    author: "Koča Popović",
+  },
+  {
+    quote: "	''Ratnički spoljni izgled srazmeran kukavičluku, terminologija i simboli (mrtvačka glava) takođe. Otkuda bi četnik mogao biti hrabar, kada osnova i osnovna pobuda njihovog stava i opredeljenja: kukavičluk, priznanje nemoći pred snagom okupatora.''",
+    author: "Koča Popović",
+  },
+ 
+];
 
 export default function Quotes() {
-    
-    
-    return (
-      <>
-        <Header/>
-        <Navbar/>
-        <div className="h-full m-20" >
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    setQuote(randomQuote.quote);
+    setAuthor(randomQuote.author);
+  };
+
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <div className="min-h-scree flex flex-col pt-20 items-center bg-gray-100">
+      <p className="mb-16 text-4xl">Press the button to get a random quote</p>
+        <div className="max-w-xl w-full bg-white rounded-lg p-6 shadow-md">
+          <button
+            className="my-4 bg-red-400 hover:bg-red-500 text-black font-bold py-2 px-4 rounded w-full"
+            onClick={getRandomQuote}
+          >
+            Get Random Quote
+          </button>
+          {/* <div className="text-2xl mb-2">
+            <FaQuoteLeft />
+          </div> */}
+          <blockquote className="text-xl leading-relaxed"> {quote} </blockquote>
+          <p className="text-gray-600 font-semibold mt-4 text-3xl"> {author}</p>
+        </div>
+      </div>
+      {/* <div className="h-full m-20" >
           <h2 className="text-center text-4xl">Famous quotes from the second world war</h2>
           <p className="text-center flex justify-center m-20 px-20">Sweet roll candy sweet roll donut pie danish cake macaroon bear claw. Soufflé donut dragée chocolate pudding icing. Chocolate cake chocolate cake tootsie roll pastry cupcake icing shortbread oat cake. Gummi bears dessert chocolate powder chocolate cake wafer dessert gummies halvah. Croissant lemon drops powder ice cream jelly beans marshmallow jelly gingerbread tootsie roll. Cookie toffee lollipop sweet roll croissant. Carrot cake jelly pudding cotton candy cake chocolate cake biscuit. Bonbon oat cake chocolate bar gummi bears tootsie roll caramels cotton candy liquorice. Jelly caramels marshmallow chocolate bar shortbread lemon drops. Lollipop shortbread chupa chups oat cake marzipan shortbread marshmallow brownie tart. Jelly-o biscuit cake gummi bears chocolate bar croissant jujubes apple pie oat cake. Sesame snaps jelly jujubes danish oat cake. Carrot cake dessert sugar plum shortbread toffee. Topping caramels cotton candy lemon drops jelly beans gummi bears sweet.</p>  
           <p className="text-center flex justify-center m-20 px-20">Sweet roll candy sweet roll donut pie danish cake macaroon bear claw. Soufflé donut dragée chocolate pudding icing. Chocolate cake chocolate cake tootsie roll pastry cupcake icing shortbread oat cake. Gummi bears dessert chocolate powder chocolate cake wafer dessert gummies halvah. Croissant lemon drops powder ice cream jelly beans marshmallow jelly gingerbread tootsie roll. Cookie toffee lollipop sweet roll croissant. Carrot cake jelly pudding cotton candy cake chocolate cake biscuit. Bonbon oat cake chocolate bar gummi bears tootsie roll caramels cotton candy liquorice. Jelly caramels marshmallow chocolate bar shortbread lemon drops. Lollipop shortbread chupa chups oat cake marzipan shortbread marshmallow brownie tart. Jelly-o biscuit cake gummi bears chocolate bar croissant jujubes apple pie oat cake. Sesame snaps jelly jujubes danish oat cake. Carrot cake dessert sugar plum shortbread toffee. Topping caramels cotton candy lemon drops jelly beans gummi bears sweet.</p>  
           <p className="text-center flex justify-center m-20 px-20">Sweet roll candy sweet roll donut pie danish cake macaroon bear claw. Soufflé donut dragée chocolate pudding icing. Chocolate cake chocolate cake tootsie roll pastry cupcake icing shortbread oat cake. Gummi bears dessert chocolate powder chocolate cake wafer dessert gummies halvah. Croissant lemon drops powder ice cream jelly beans marshmallow jelly gingerbread tootsie roll. Cookie toffee lollipop sweet roll croissant. Carrot cake jelly pudding cotton candy cake chocolate cake biscuit. Bonbon oat cake chocolate bar gummi bears tootsie roll caramels cotton candy liquorice. Jelly caramels marshmallow chocolate bar shortbread lemon drops. Lollipop shortbread chupa chups oat cake marzipan shortbread marshmallow brownie tart. Jelly-o biscuit cake gummi bears chocolate bar croissant jujubes apple pie oat cake. Sesame snaps jelly jujubes danish oat cake. Carrot cake dessert sugar plum shortbread toffee. Topping caramels cotton candy lemon drops jelly beans gummi bears sweet.</p>  
           <p className="text-center flex justify-center m-20 px-20">Sweet roll candy sweet roll donut pie danish cake macaroon bear claw. Soufflé donut dragée chocolate pudding icing. Chocolate cake chocolate cake tootsie roll pastry cupcake icing shortbread oat cake. Gummi bears dessert chocolate powder chocolate cake wafer dessert gummies halvah. Croissant lemon drops powder ice cream jelly beans marshmallow jelly gingerbread tootsie roll. Cookie toffee lollipop sweet roll croissant. Carrot cake jelly pudding cotton candy cake chocolate cake biscuit. Bonbon oat cake chocolate bar gummi bears tootsie roll caramels cotton candy liquorice. Jelly caramels marshmallow chocolate bar shortbread lemon drops. Lollipop shortbread chupa chups oat cake marzipan shortbread marshmallow brownie tart. Jelly-o biscuit cake gummi bears chocolate bar croissant jujubes apple pie oat cake. Sesame snaps jelly jujubes danish oat cake. Carrot cake dessert sugar plum shortbread toffee. Topping caramels cotton candy lemon drops jelly beans gummi bears sweet.</p>  
        
-        </div>
-      </>
-    )
-};
+        </div> */}
+    </>
+  );
+}
